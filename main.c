912f54a4b8a33ff170d70576df57a5b039bf4819
Anton Malynevskyi <malina.elfg@gmail.com>
2024-04-29 23:01:47 +0200

    Intial

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+typedef enum coin { PENNY, NICKEL, DIME, QUARTER, ERROR } coin;
+
+coin input_enum(int fm);
+
+void print_enum(coin fmoney);
+
+int main(void) {
+    coin money;
+    int choice;
+    puts("Input your choice:\n1) Penny\n2) Nickel\n3) Dime\n4) Quarter");
+    scanf("%d", &choice);
+    if (choice <= 4 && choice >= 1) {
+        money = input_enum(choice);
+        print_enum(money);
+    } else {
+        puts("Wrong choice");
+    }
+    return 0;
+}
+
+coin input_enum(int fm) {
+    coin fmoney; // змінна типу enum
+    switch (fm) {
+        case 1:
+            fmoney = PENNY;
+            break;
+
+        case 2:
+            fmoney = NICKEL;
+            break;
+
+        case 3:
+            fmoney = DIME;
+            break;
+        case 4:
+            fmoney = QUARTER;
+            break;
+        default:
+            puts("Помилка");
+            fmoney = ERROR;
+    }
+    return fmoney;
+}
+
+void print_enum(coin fmoney) {
+    switch (fmoney) {
+        case PENNY: puts("Пенні");
+            break;
+        case NICKEL: puts("Нікель");
+            break;
+        case DIME: puts("Монета в 10 центів");
+            break;
+        case QUARTER: puts("Чверть доллара");
+            break;
+        default: puts("Помилка");
+    }
+}
